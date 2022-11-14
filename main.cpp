9e90de0cf250ac60352c6c9bd74f86f158387a31
Damilomba03 <115529057+Damilomba03@users.noreply.github.com>
2022-11-14 13:48:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    cout << "Hello, World!" << endl;
+    float a=0,b=0,x=0,y=0;
+    cin>>a>>b>>x>>y;
+    if((x<0)&&(y>0))
+    {
+        cout<<((a*x)-(b*y))<<endl;
+    }
+    else if((x>=0)&&(y<=0))
+    {
+        cout<<((a*x*x)-(b*y))<<endl;
+    }
+    else
+    {
+        cout<<((a*x)+(b*y*y))<<endl;
+    }
+
+    return 0;
 }
